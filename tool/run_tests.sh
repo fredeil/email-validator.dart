180806d42915532ca877227695ef1290f8aa232a
fredeil <fredrik.eilertsen@gmail.com>
2018-08-31 23:43:20 +0200

    Rename test file

--- tool/run_tests.sh
+++ tool/run_tests.sh
@@ -5,6 +5,6 @@ set -e
 DIR=$( cd $( dirname "${BASH_SOURCE[0]}" )/.. && pwd )
 
 echo "Analyzing library for warnings or type errors"
-pub run test $DIR/test/test.dart
+pub run test $DIR/test/email_validator_test.dart
 
 echo -e "\n[32m✓ OK[0m"
