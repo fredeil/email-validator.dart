bfc0351065680c756d19b999da32a5ec20887844
fredeil <fredrik.eilertsen@gmail.com>
2018-09-05 17:53:53 +0200

    Update example for new change

--- example/example.dart
+++ example/example.dart
@@ -2,8 +2,8 @@ import 'dart:core';
 import 'package:email_validator/email_validator.dart';
 
 void main() {
-    const String email = "fredrik.eilertsen@gmail.com";
-    final bool isValid = EmailValidator.Validate(email);
+    const String email = 'fredrik.eilertsen@gmail.com';
+    final bool isValid = EmailValidator.validate(email);
 
     print('Email is valid? ' + (isValid ? 'yes' : 'no'));
 }
