5e2efa0a391c2cae5bea5ec0e5cba777e2e1a2bf
Fredrik Eilertsen <fredrik.eilertsen@gmail.com>
2022-07-24 13:39:40 +0200

    Format

--- example/example.dart
+++ example/example.dart
@@ -2,8 +2,8 @@ import 'dart:core';
 import 'package:email_validator/email_validator.dart';
 
 void main() {
-    const String email = 'fredrik.eilertsen@gail.com';
-    final bool isValid = EmailValidator.validate(email);
+  const String email = 'fredrik.eilertsen@gail.com';
+  final bool isValid = EmailValidator.validate(email);
 
-    print('Email is valid? ' + (isValid ? 'yes' : 'no'));
+  print('Email is valid? ' + (isValid ? 'yes' : 'no'));
 }
